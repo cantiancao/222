8dba2aa5f45620966abc8663d2792f2da6e43aaf
cantiancao <1171187296@qq.com>
2019-04-11 23:12:21 +0800

    1.2ceshi

--- mymenu/AppDelegate.m
+++ mymenu/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor redColor];
+    UIImage *imag = [UIImage new];
     HCMainTabbarController *tabbar = [[HCMainTabbarController alloc]init];
     self.window.rootViewController = tabbar;
     [self.window makeKeyAndVisible];
